b62192c52304b1d559b7e266b7b71eab87687b17
Jordao Bragantini <jordao.bragantini@czbiohub.org>
2024-05-15 11:48:55 -0700

    changed doc themes and updated faq

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,8 +19,8 @@ sys.path.insert(0, os.path.abspath("../.."))
 # -- Project information -----------------------------------------------------
 
 project = "ultrack"
-copyright = "2023, Jordão Bragantini"
-author = "Jordão Bragantini"
+copyright = "2024, Jordão Bragantini"
+author = "Jordão Bragantini & contributors"
 
 # The full version, including alpha/beta/rc tags
 release = libversion("ultrack")
@@ -45,7 +45,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx_rtd_theme"
+html_theme = "furo"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
